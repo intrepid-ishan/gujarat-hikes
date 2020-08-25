8d4b4fe435dc07d24576a4e63b834a6309968986
ISHAN MAKADIA <ishan.makadia@gmail.com>
2020-08-26 01:26:15 +0530

    Update app.js: Comment Routes added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var  express    = require("express"),
      bodyParser = require('body-parser'),
      mongoose   = require("mongoose"),
      Campground = require("./models/campground"),
-     seedDB     = require("./seeds");
-
+     seedDB     = require("./seeds"),
+     Comment    = require("./models/comment");
 
 seedDB();
 
@@ -13,38 +13,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine","ejs");
 
 
-// Campground.create(
-//     {
-//         name:"Polo Forest",
-//         image:"https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-//         description: "Located in Gujarat!"
-//     },
-//     function(err,campground){
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             console.log("Newly Created Campground");
-//             console.log(campground);
-//         }
-//      });
-
 
 app.get("/",(req,res)=>{
      res.render("landing");
 }); 
 
+//INDEX
 app.get("/campgrounds",(req,res)=>{ 
     Campground.find({},function(err,allCampgrounds){
         if(err){
             console.log(err);
         }
         else{
-            res.render("index",{campgrounds:allCampgrounds});
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
         }
     });
 });
 
+//NEW
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("campgrounds/new");
+});
+
+//CREATE
 app.post("/campgrounds",(req,res)=>{
     var name = req.body.name;
     var image = req.body.image;
@@ -65,10 +56,6 @@ app.post("/campgrounds",(req,res)=>{
     });    
 });
 
-app.get("/campgrounds/new",(req,res)=>{
-    res.render("new");
-});
-
 //SHOW - shows more info about one campground   
 app.get("/campgrounds/:id",function(req,res){
     Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
@@ -76,12 +63,49 @@ app.get("/campgrounds/:id",function(req,res){
             console.log(err);
         } else{
             console.log(foundCampground);
-            res.render("show",{campground: foundCampground});
+            res.render("campgrounds/show",{campground: foundCampground});
         }
     });
 });
 
 
+//================
+// COMMENTS ROUTES
+//================
+
+//NEW
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    Campground.findById(req.params.id,function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new",{campground: campground});
+        }
+    });
+});
+
+//CREATE
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+         Comment.create(req.body.comment, function(err, comment){
+            if(err){
+                console.log(err);
+            } else {
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect('/campgrounds/' + campground._id);
+            }
+         });
+        }
+    });
+ });
+
+
+ 
 app.listen(3000, ()=>{
     console.log("Yelp Camp Server has started!");
 });
