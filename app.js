2613b0a66f1cf89230f42c7bbd4cc3335185ac4e
Ishan <coderishan@gmail.com>
2020-09-17 10:48:43 +0530

    add remote DB

--- app.js
+++ app.js
@@ -36,12 +36,23 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-mongoose.connect("mongodb://localhost:27017/gujarat-hikes",
-    {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    }
-);
+// mongoose.connect("mongodb://localhost:27017/gujarat-hikes",
+//     {
+//         useNewUrlParser: true, 
+//         useUnifiedTopology: true
+//     }
+// );
+
+mongoose.connect("mongodb+srv://Anand:Petikar@cluster0.wfbnr.mongodb.net/<dbname>?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(()=>{
+    console.log('Connected to DB!');
+}).catch(err => {
+    console.log('ERROR:',err.message);
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
