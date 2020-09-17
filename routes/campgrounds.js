30211f8ff310d69a56c939e9d27a75314c4f835b
ISHAN MAKADIA <coderishan@gmail.com>
2020-09-17 08:06:19 +0530

    Add Dynamic Price

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,6 +24,7 @@ router.get("/new",middleware.isLoggedIn,(req,res)=>{
 //CREATE
 router.post("/",middleware.isLoggedIn,(req,res)=>{
    var name = req.body.name;
+   var price = req.body.price;
    var image = req.body.image;
    var desc = req.body.description;
    var author = {
@@ -32,6 +33,7 @@ router.post("/",middleware.isLoggedIn,(req,res)=>{
    }
    var newCampground = {
                        name:name,
+                       price: price,
                        image:image,
                        description: desc,
                        author: author 
